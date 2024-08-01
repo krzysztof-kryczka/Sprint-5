35d9d399d6b8d330e477f86c1a74766671a801e7
Krzysztof Kryczka <kw.kryczka@gmail.com>
2024-08-01 20:46:58 +0200

    Input data - array of people objects

---
+++ app.js
@@ -0,0 +1,27 @@
+const inputDataPeople = [
+    {
+        firstName: false,
+        lastName: 2,
+    },
+    {
+        firstName: 'Roman',
+        lastName: 'Kowalska',
+    },
+    {
+        firstName: 'Halina',
+        lastName: 'Malina',
+    },
+    {
+        firstName: 'B',
+        lastName: '22',
+    },
+    {
+        firstName: 'Jan',
+        lastName: 'Nowak',
+    },
+    {
+        firstName: 'Kamil',
+        lastName: null,
+    },
+]
+
